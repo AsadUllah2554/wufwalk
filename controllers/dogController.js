69893ad297790c9450d7602c96e27f39c0ba67cf
Asad <axsad398@gmail.com>
2023-09-28 22:16:01 +0500

    Update dogController.js

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -1,21 +1,19 @@
 const User = require('../models/userModel')
 
-
-
     const getDogs = async (req, res) => {
-        const { username } = req.params; // Assuming you are passing the username as a parameter
+        const { userID } = req.params; // Assuming you are passing the username as a parameter
       
         try {
           // Find the user by username
-          const user = await User.findOne({ username });
-      
+        const user = await User.findById(userID);
+          console.log(user)
           if (!user) {
             return res.status(404).json({ msg: "User not found" });
           }
           // Access the user's dogs array from the user object
           const userDogs = user.dogs;
       
-          res.status(200).json(userDogs);
+          res.status(200).json(user,userDogs);
         } catch (error) {
           res.status(500).json({ error: error.message });
         }
